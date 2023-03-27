685fb60d2f0bcef72d68ed85cc94554c18ef5c6c
chrysanthemum <asingiser@gmail.com>
2023-03-27 12:40:24 -0400

    test and fix ComfortLevels

--- src/ComfortLevels.js
+++ src/ComfortLevels.js
@@ -3,12 +3,11 @@
 function ComfortLevels(temp) {
     return temp < 50
     ? 'Really Cold'
-    : temp > 50 && temp < 61
+    : temp >= 50 && temp < 61
     ? 'Chilly'
-    : temp > 61 && temp < 80
+    : temp >= 61 && temp < 80
     ? 'Ideal'
-    : temp > 80 && temp < 89
+    : temp >= 80 && temp < 89
     ? 'Warm'
     : 'Really Hot'     
 }
-
