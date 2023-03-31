908f8e891e0ad9a2544daf1c78d6655c7cf71b0a
Anthony <asingiser@gmail.com>
2023-03-31 13:05:55 -0400

    commit changes

--- src/LatLong.js
+++ src/LatLong.js
@@ -2,10 +2,11 @@ export default function LatLong () {
     navigator.geolocation.getCurrentPosition((position) => {
         return (
             <>
+                {console.log(position.coords.latitude)}
+                {console.log(position.coords.longitude)}
                 {position.coords.latitude}
                 {position.coords.longitude}
             </>
         )
-    }
-    )
+    })
 }
